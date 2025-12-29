a5c06e5756345114985007a6213cba9709652d7a
Kaivalyakulkarni <kaivalyakulkarni2011@gmail.com>
2025-12-29 20:54:11 +0530

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) # Enable CORS for all routes
+
+@app.route('/api/users', methods=['GET'])
+def users():
+    return jsonify({"users": ["Krrish", "Ishika", "Sanket"]})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
