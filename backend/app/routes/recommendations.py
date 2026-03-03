dd4e7c22ece6ec1d015b252abc26ad5ac413ac15
Kaivalyakulkarni <kaivalyakulkarni2011@gmail.com>
2026-03-03 13:02:20 +0530

    feat:boilerplate for recommendations completed

--- backend/app/routes/recommendations.py
+++ backend/app/routes/recommendations.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 recommendations_bp = Blueprint('recommendations', __name__)
 
 @recommendations_bp.route('/', methods=['GET'])
+@jwt_required()
 def get_recommendations():
-    return jsonify({"message": "Recommendations route working!"})
+    current_user = get_jwt_identity()
+    #ml logic will be here
+    return jsonify({"message":"Recommendation comming soon","user": current_user})
+    
